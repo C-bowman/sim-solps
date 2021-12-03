df9f3420b918913b0f8dd8c0d722665eb2d8fd22
Chris Bowman <chris.bowman.physics@gmail.com>
2021-12-03 01:17:08 +0000

    SolpsInterface now directly splits SOLPS cells into triangles

--- sims/interface.py
+++ sims/interface.py
@@ -1,5 +1,5 @@
 
-from numpy import array, diff, meshgrid, arange, concatenate, zeros
+from numpy import array, concatenate, zeros, full
 from scipy.io import netcdf
 from tokamesh import TriangularMesh
 from tokamesh.construction import remove_duplicate_vertices
@@ -16,88 +16,90 @@ class SolpsInterface(object):
         A path to a balance.nc file.
     """
     def __init__(self, balance_filepath):
-
         with netcdf.netcdf_file(balance_filepath, 'r') as solps:
-            # get needed info
             self.ne = solps.variables['ne'].data.flatten()
             self.te = solps.variables['te'].data.flatten() / 1.602e-19
             self.ti = solps.variables['ti'].data.flatten() / 1.602e-19
+            self.n_cells = self.ne.size
 
             den_i = solps.variables['na'].data
             den_n = solps.variables['dab2'].data
             self.n0 = den_n[:, :, 0:den_i.shape[-1]].flatten()
             # find the cell centres
-            R_cc = solps.variables['crx'].data.mean(axis=0)
-            z_cc = solps.variables['cry'].data.mean(axis=0)
+            crx = solps.variables['crx'].data.copy()
+            cry = solps.variables['cry'].data.copy()
+            R_sets = [v for v in crx.reshape([4,self.n_cells]).T]
+            z_sets = [v for v in cry.reshape([4,self.n_cells]).T]
             # get the indices used to define the sub-grids
-            cuts = (diff(solps.variables['leftix'][0,:]) < 1).nonzero()[0]
-            sep_ind = solps.variables['jsep'].data[0] + 1
             del den_n, den_i
 
-        if cuts.size != 5:
-            raise ValueError(
-                """
-                [ SolpsInterface error ]
-                >> Currently only the 'connected double null' configuration
-                >> is supported - the given balance file was of an unsupported
-                >> configuration.
-                """
-            )
-
-        self.grid_shape = R_cc.shape
-        self.submesh_slices = {
-            'lower inner leg': (slice(None, None), slice(None, cuts[0])),
-            'upper inner leg': (slice(None, None), slice(cuts[1], cuts[2] + 1)),
-            'upper outer leg': (slice(None, None), slice(cuts[2] + 1, cuts[3] + 1)),
-            'lower outer leg': (slice(None, None), slice(cuts[4] + 1, None)),
-            'lfs core': (slice(None, sep_ind + 1), slice(cuts[0], cuts[1])),
-            'lfs sol': (slice(sep_ind, None), slice(cuts[0] - 1, cuts[1] + 1)),
-            'hfs core': (slice(None, sep_ind + 1), slice(cuts[3] + 1, cuts[4] + 1)),
-            'hfs sol': (slice(sep_ind, None), slice(cuts[3], cuts[4] + 2)),
-            'upper pfr connector': tuple(meshgrid(arange(sep_ind+1), array([cuts[1], cuts[3]]))),
-            'upper core connector': tuple(meshgrid(arange(sep_ind+1), array([cuts[1]-1, cuts[3]+1]))),
-            'lower core connector': tuple(meshgrid(arange(sep_ind+1), array([cuts[0], cuts[4]]))),
-            'lower pfr connector': tuple(meshgrid(arange(sep_ind+1), array([cuts[0]-1, cuts[4]+1]))),
-            'upper x-point connector': (array([[sep_ind, sep_ind],[sep_ind, sep_ind]]), array([[cuts[1]-1, cuts[1]],[cuts[3]+1, cuts[3]]])),
-            'lower x-point connector': (array([[sep_ind, sep_ind],[sep_ind, sep_ind]]), array([[cuts[0]-1, cuts[0]],[cuts[4]+1, cuts[4]]]))
-        }
-
         R, z, triangles = connect_meshes(
-            [R_cc[s].flatten() for s in self.submesh_slices.values()],
-            [z_cc[s].flatten() for s in self.submesh_slices.values()],
-            [triangles_from_grid(R_cc[s].shape) for s in self.submesh_slices.values()]
+            R_sets,
+            z_sets,
+            [triangles_from_grid([2,2])]*self.n_cells
         )
 
-        # to unscramble the mesh R and z, first find the mapping from the order
-        # obtained from sorting by z then by R, to the original permutation
-        inverse_sort = double_argsort(R_cc.flatten(), z_cc.flatten()).argsort()
-        # now combine this with the sorting map for the grid R and z to get
-        # a mapping straight to the original ordering
-        descrambler = double_argsort(R, z)[inverse_sort]
-        # now re-order the mesh data before building the mesh
-        triangles = descrambler.argsort()[triangles]
-        self.mesh = TriangularMesh(R_cc.flatten(), z_cc.flatten(), triangles)
+        old_Rc = zeros(2 * self.n_cells)
+        old_Rc[0::2] = crx[array([0, 1, 2]), :].mean(axis=0).flatten()
+        old_Rc[1::2] = crx[array([1, 2, 3]), :].mean(axis=0).flatten()
 
-    def get(self, variable, R, z):
-        return self.mesh.interpolate(R, z, vertex_values=getattr(self, variable))
+        old_zc = zeros(2 * self.n_cells)
+        old_zc[0::2] = cry[array([0, 1, 2]), :].mean(axis=0).flatten()
+        old_zc[1::2] = cry[array([1, 2, 3]), :].mean(axis=0).flatten()
 
-    def build_submeshes(self):
-        R = self.mesh.R.reshape(self.grid_shape)
-        z = self.mesh.z.reshape(self.grid_shape)
+        new_Rc = R[triangles].mean(axis=1)
+        new_zc = z[triangles].mean(axis=1)
 
-        return {key: TriangularMesh(
-            R[slc].flatten(),
-            z[slc].flatten(),
-            triangles_from_grid(R[slc].shape)
-            ) for key, slc in self.submesh_slices.items()
-        }
+        inverse_sort = double_argsort(old_Rc, old_zc).argsort()
+        descrambler = double_argsort(new_Rc, new_zc)[inverse_sort]
+        triangles = triangles[descrambler,:]
+        self.mesh = TriangularMesh(R=R, z=z, triangles=triangles)
 
-    def plot(self, variable):
-        r_axis, z_axis, image = self.mesh.get_field_image(getattr(self, variable), shape=(256,512))
-        fig = plt.figure(figsize=(10*(r_axis.ptp()/z_axis.ptp())*1.2, 10))
+    def get(self, variable, R, z, outside_value=0):
+        """
+        Returns the value of a chosen variable at a given set of points.
+
+        :param variable: \
+            A string indicating which variable to retrieve.
+
+        :param R: \
+            The radius values of the points at which the variable is determined.
+
+        :param z: \
+            The z-height values of the points at which the variable is determined.
+
+        :param outside_value: \
+            The value which is assigned to any points which lie outside the SOLPS grid.
+
+        :return: \
+            The values of the variable at the given points as a 1D numpy array.
+        """
+        inds = self.mesh.find_triangle(R, z)
+        inside = inds != -1
+        values = full(R.size, fill_value=outside_value)
+        values[inside] = getattr(self, variable)[inds//2][inside]
+        return values
+
+    def plot(self, variable, draw_mesh=False):
+        """
+        Generate a colour plot of a chosen variable.
+
+        :param str variable: A string indicating which variable to plot.
+        :param bool draw_mesh: A bool indicating whether to draw the mesh in the plot.
+        """
+        vals = zeros(2*self.n_cells)
+        vals[0::2] = getattr(self, variable)
+        vals[1::2] = getattr(self, variable)
+        cmap = get_cmap('viridis')
+
+        dR = self.mesh.R_limits[1] - self.mesh.R_limits[0]
+        dz = self.mesh.z_limits[1] - self.mesh.z_limits[0]
+        fig = plt.figure(figsize=(10*(dR/dz)*1.3, 10))
         ax = fig.add_subplot(111)
-        ax.set_facecolor(get_cmap('viridis')(0.))
-        ax.contourf(r_axis, z_axis, image.T, 64)
+        ax.set_facecolor(cmap(0.))
+        ax.tripcolor(self.mesh.R, self.mesh.z, self.mesh.triangle_vertices, facecolors=vals)
+        if draw_mesh:
+            self.mesh.draw(ax, lw=0.5)
         ax.axis('equal')
         ax.set_xlabel('R (m)')
         ax.set_ylabel('z (m)')
